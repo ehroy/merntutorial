00c982276f0374d71e83dc6db845a0f7ceb144fa
ehroy <ahmadroy092@gmail.com>
2022-07-04 20:02:36 -0700

    first commit

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -14,7 +14,28 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw Error("User Already exists");
   }
-  res.json({ message: "Register User " });
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(200).json({
+      __id: user["id"],
+      name: user["name"],
+      email: user["email"],
+    });
+  } else {
+    res.status(400);
+    throw Error("Invalid user data");
+  }
+
+  //   res.json({ message: "Register User " });
 });
 
 const loginUser = asyncHandler(async (req, res) => {
