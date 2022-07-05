0b29b49091795e27281c3b6d7ec189b832ef7462
ehroy <ahmadroy092@gmail.com>
2022-07-04 19:42:25 -0700

    first commit

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -2,4 +2,11 @@ const registerUser = (req, res) => {
   res.json({ message: "Register User " });
 };
 
-module.exports = { registerUser };
+const loginUser = (req, res) => {
+  res.json({ message: "Login User " });
+};
+const getMe = (req, res) => {
+  res.json({ message: "User Display " });
+};
+
+module.exports = { registerUser, loginUser, getMe };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controller/usercontroller");
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controller/usercontroller");
 
 router.post("/", registerUser);
+router.post("/login", loginUser);
+router.post("/me", getMe);
 
 module.exports = router;
